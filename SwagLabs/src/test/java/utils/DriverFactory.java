9c60178d15b0f87b3b7d911173506773adca2219
Jason Kraft <jasondominickraft@gmail.com>
2026-04-14 15:15:42 +0200

    Updated DriverFavtory to ensure tests run headless

--- SwagLabs/src/test/java/utils/DriverFactory.java
+++ SwagLabs/src/test/java/utils/DriverFactory.java
@@ -14,11 +14,13 @@ public class DriverFactory {
 
         if (browser.equalsIgnoreCase("chrome")){
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");
+            options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
 
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
             driver = new EdgeDriver();
         } else {
